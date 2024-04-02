087739f4d7669f065972f2ebeecace11f92dc709
Ichiky_Otsu <66952647+Brice66@users.noreply.github.com>
2024-04-03 01:02:42 +0200

    productphp

--- web/pages/www/products.php
+++ web/pages/www/products.php
@@ -1,57 +1,44 @@
-<!DOCTYPE html>
 <html>
+<style>
+	table,
+	th,
+	td {
+		padding: 10px;
+		border: 1px solid black;
+		border-collapse: collapse;
+	}
+</style>
+
 <head>
-    <title>Catalogue WoodyToys</title>
-    <style>
-        table, th, td {
-            padding: 10px;
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-    </style>
+<title>Catalogue WoodyToys</title>
 </head>
+
 <body>
-    <h1>Catalogue WoodyToys</h1>
+<h1>Catalogue WoodyToys</h1>
 
-    <?php
-    // Informations de connexion à la base de données
-    $dbname = 'woodytoys';
-    $dbuser = 'root';
-    $dbpass = 'mypass';
-    $dbhost = '172.17.0.4';
-
-    // Connexion à la base de données
-    $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-    if (!$connect) {
-        die("Connexion échouée: " . mysqli_connect_error());
-    }
-
-    // Sélection de la base de données (pas nécessaire si vous utilisez le quatrième paramètre de mysqli_connect)
-    // mysqli_select_db($connect, $dbname) or die("Could not open the database '$dbname'");
-
-    // Requête pour obtenir les produits
-    $query = "SELECT id, product_name, product_price FROM products";
-    $result = mysqli_query($connect, $query);
-    if (!$result) {
-        die("Requête échouée: " . mysqli_error($connect));
-    }
-    ?>
+<?php
+$dbname = getenv('MYSQL_DATABASE');
+$dbuser = getenv('MYSQL_USER');
+$dbpass = getenv('MYSQL_PASSWORD');
+$dbhost = getenv('MYSQL_HOST');
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
+mysqli_select_db($connect,$dbname) or die("Could not open the database '$dbname'");
+$result = mysqli_query($connect,"SELECT id, name, price FROM products");
+?>
 
     <table>
-        <tr>
-            <th>Numéro de produit</th>
-            <th>Descriptif</th>
-            <th>Prix</th>
-        </tr>
+    <tr>
+    <th>Numéro de produit</th>
+    <th>Descriptif</th>
+    <th>Prix</th>
+</tr>
 
-        <?php
-        // Affichage des lignes du tableau
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>" . htmlspecialchars($row['id']) . "</td><td>" . htmlspecialchars($row['product_name']) . "</td><td>" . htmlspecialchars($row['product_price']) . "</td></tr>";
-        }
-        ?>
+<?
+while ($row = mysqli_fetch_array($result)) {
+	printf("<tr><th>%s</th> <th>%s</th> <th>%s</th></tr>",$row[0],$row[1],$row[2]);
+}
+?>
 
-    </table>
+</table>
 </body>
 </html>
-
