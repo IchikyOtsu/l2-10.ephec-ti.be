e13a8230198e7f150d52cc15f109d2b850d23d2c
Ichiky_Otsu <66952647+IchikyOtsu@users.noreply.github.com>
2024-04-15 13:13:04 +0200

    Update script.js

--- web/pages/www/script.js
+++ web/pages/www/script.js
@@ -2,16 +2,10 @@ const title = document.querySelector('.title');
 const lines = document.querySelectorAll('.line');
 
 let titleRotation = 0;
-let linesRotation = 0;
 
 function rotateElements() {
   titleRotation += 0.01;
-  linesRotation += 0.005;
-
   title.style.transform = `rotateY(${titleRotation}turn)`;
-  lines.forEach(line => {
-    line.style.transform = `rotateX(${linesRotation}turn)`;
-  });
 
   requestAnimationFrame(rotateElements);
 }
